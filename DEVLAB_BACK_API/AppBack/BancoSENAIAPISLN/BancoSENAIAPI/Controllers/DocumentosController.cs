382ae0a38ddf22ee87692c087049521e8e21b10b
luizcardoso44 <luiz.g.santos26@aluno.senai.br>
2026-09-11 09:08:22 -0300

    Feat(documento): definição metadados no upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -25,6 +25,11 @@ namespace BancoSENAIAPI.Controllers
             {
                 Directory.CreateDirectory(pastaCliente);
             }
+            string extensao = Path.GetExtension(arquivo.FileName);
+            string nameOriginal = Path.GetFileNameWithoutExtension(arquivo.FileName);
+            string novonome = $"{codigoCliente}_{nameOriginal}_{Guid.NewGuid()}{extensao}";
+            string caminhoFinal = Path.Combine(pastaCliente, novonome);
+
         }
     }
 }
