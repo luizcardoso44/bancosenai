4cd1712ec3dc7657f0604ebb0904c20833061e40
luizcardoso44 <luiz.g.santos26@aluno.senai.br>
2026-09-11 08:37:19 -0300

    Feat(documentos): Simulação do banco de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -6,6 +6,10 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentosController : Controller
     {
+        private readonly string _caminhoraiz = Path.Combine(Directory.GetCurrentDirectory(), "ClienteArquivos");
 
+        private static List<Models.DocumentoMetadados> _documentosMetadados = new List<Models.DocumentoMetadados>();
+
+        private static int _nextId = 1;
     }
 }
--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadados.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadados.cs
@@ -2,5 +2,10 @@
 {
     public class DocumentoMetadados
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Extensão { get; set;}
+        public string Caminho { get; set;}
+        public int CodigoCliente { get; set;}
     }
 }
