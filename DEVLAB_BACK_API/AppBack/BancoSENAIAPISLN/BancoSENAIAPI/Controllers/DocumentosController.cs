f6e275194df4f6f081940b0ec4813b902ea89f5d
luizcardoso44 <luiz.g.santos26@aluno.senai.br>
2026-09-14 07:54:55 -0300

    feat(documentos): exclusao

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -73,6 +73,25 @@ namespace BancoSENAIAPI.Controllers
 
             return File(fileBytes, "application/octet-stream", nomeArquivo);
         }
+        [HttpDelete("/api/v1/documento/excluir/{id}")]
+        public IActionResult ExcluirDocumento(int id)
+        {
+            var documento = _documentosMetadados.FirstOrDefault(d => d.Id == id);
+
+            if (documento == null)
+            {
+                return NotFound("Documento não encontrado.");
+            }
+
+            if (System.IO.File.Exists(documento.Caminho))
+            {
+                System.IO.File.Delete(documento.Caminho);
+            }
+
+            _documentosMetadados.Remove(documento);
+
+            return Ok(new { mensagem = "Documento excluído com sucesso." });
+        }
     }
 }
 
